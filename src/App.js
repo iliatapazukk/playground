2b17052ecea2e46f3a47466b32bfec8c59f30b83
iliatapazukk <mozesinambient@gmail.com>
2021-03-14 11:14:28 +0300

    ReactMarathon_homework_vol3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import Header from "./components/Header";
-import Layout from "./components/Layout";
-import Footer from "./components/Footer";
-import backgroundFirst from "./assets/img/Bg1.jpg"
-import backgroundSecond from "./assets/img/Bg2.jpg"
-import './App.css';
+import Header from './components/Header'
+import Layout from './components/Layout'
+import Footer from './components/Footer'
+import PokemonCard from './components/PokemonCard'
+import POKEMONS from './Pokemons'
+import backgroundFirst from './assets/img/Bg1.jpg'
+import backgroundSecond from './assets/img/Bg2.jpg'
+import inlineImg from './assets/img/logo.svg'
 
+import './App.css';
 import './index.css'
 
 function App() {
@@ -17,22 +20,45 @@ function App() {
       <Layout
         urlBg={backgroundFirst}
         title="Consectetur adipiscing"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore"
-      />
+      >
+      </Layout>
 
       <Layout
-        colorBg = '#d3dfe7'
+        colorBg='#d3dfe7'
         title="Consectetur adipiscing"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore"
-      />
+      >
+        <div className="flex">
+          {POKEMONS.map(item =>
+            <PokemonCard
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              type ={item.type}
+              img ={item.img}
+              values ={item.values}
+            />
+          )}
+        </div>
+
+      </Layout>
       <Layout
         urlBg={backgroundSecond}
         title="Consectetur adipiscing"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore"
-      />
+      >
+        <img src={inlineImg} alt=""/>
+        <p>In the game two players face off against one another, one side playing as "blue", the other as "red" on a 3x3
+          grid.
+          Each player has five cards in a hand and the aim is to capture the opponent's cards by turning them into the
+          player's own color of red or blue.</p>
+
+        <p>To win, a majority of the total ten cards played (including the one card that is not placed on the board)
+          must be of the player's card color. To do this, the player must capture cards by placing a card adjacent to an
+          opponent's card whereupon the 'ranks' of the sides where the two cards touch will be compared. If the rank of
+          the opponent's card is higher than the player's card, the player's card will be captured and turned into the
+          opponent's color. If the player's rank is higher, the opponent's card will be captured and changed into the
+          player's color instead.</p>
+
+      </Layout>
       <Footer/>
     </>
   );
