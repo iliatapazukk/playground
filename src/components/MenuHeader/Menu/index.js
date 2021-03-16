8c1006a11c6dfe65f178c7023152ff5df0a0dc01
iliatapazukk <mozesinambient@gmail.com>
2021-03-16 20:56:46 +0300

    remove menu blink

--- src/components/MenuHeader/Menu/index.js
+++ src/components/MenuHeader/Menu/index.js
@@ -1,36 +1,39 @@
 import style from './style.module.css'
 import cn from 'classnames'
 
-const Menu = ({isActive}) => {
+const Menu = ({isActive = false}) => {
   return (
-    //active/deactive
-    <div className={cn(style.menuContainer, {[style.active]:isActive, [style.deactive]:!isActive})}>
-      <div className={style.overlay}/>
-      <div className={style.menuItems}>
-        <ul>
-          <li>
-            <a href="#welcome">
-              HOME
-            </a>
-          </li>
-          <li>
-            <a href="#game">
-              GAME
-            </a>
-          </li>
-          <li>
-            <a href="#about">
-              ABOUT
-            </a>
-          </li>
-          <li>
-            <a href="#contact">
-              CONTACT
-            </a>
-          </li>
-        </ul>
+    <>
+      { isActive && (
+      <div className={cn(style.menuContainer, {[style.deactive]:!isActive, [style.active]:isActive})}>
+        <div className={style.overlay}/>
+        <div className={style.menuItems}>
+          <ul>
+            <li>
+              <a href="#welcome">
+                HOME
+              </a>
+            </li>
+            <li>
+              <a href="#game">
+                GAME
+              </a>
+            </li>
+            <li>
+              <a href="#about">
+                ABOUT
+              </a>
+            </li>
+            <li>
+              <a href="#contact">
+                CONTACT
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+      )}
+    </>
   )
 }
 
